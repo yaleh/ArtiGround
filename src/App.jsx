695e21f74f8464d1dca9d95c98862459bc79e5ea
Yale Huang <calvino.huang@gmail.com>
2024-10-10 02:14:01 +0000

    Added file explorer to Sandpack.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import SandpackEditor from './SandpackEditor'
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const [selectedTemplate, setSelectedTemplate] = useState('react-ts')
+
+  const templates = [
+    'static',
+    'angular',
+    'react',
+    'react-ts',
+    'solid',
+    'svelte',
+    'vanilla',
+    'vanilla-ts',
+    'vue',
+    'vue-ts'
+  ]
   return (
     <>
       <h1>Vite + React</h1>
-      <SandpackEditor />
+      <div className="template-selector">
+        <label htmlFor="template-select">Select Template: </label>
+        <select
+          id="template-select"
+          value={selectedTemplate}
+          onChange={(e) => setSelectedTemplate(e.target.value)}
+        >
+          {templates.map((template) => (
+            <option key={template} value={template}>
+              {template}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="sp-wrapper">
+        <SandpackEditor template={selectedTemplate} />
+      </div>
     </>
   )
 }
